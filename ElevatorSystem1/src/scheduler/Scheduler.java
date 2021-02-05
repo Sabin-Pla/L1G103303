02089fff6414270541aa40efbb3f258830150a94
Aayush Mallya <aayushmallya72@gmail.com>
2021-02-05 16:06:14 -0500

    Scheduler

--- ElevatorSystem1/src/scheduler/Scheduler.java
+++ ElevatorSystem1/src/scheduler/Scheduler.java
@@ -3,21 +3,31 @@ package scheduler;
 import floorSubSystem.DataStorage;
 
 /**
- *
+ * @author Aayush Mallya & Harshil Verma
  */
 public class Scheduler implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while(true){
+			setRequest(data);
+			try{
+				Thread.sleep(500);
+			} catch (InterruptExecption e){
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public DataStorage getRequest() {
-		//return 
+		getRequestTime();
+		getCurrentFloor();
+		getGoingUp();
+		getDestinationFloor();
 	}
 	
 	public synchronized void setRequest(DataStorage data) {
+		this.data = data;
 		
 	}
 
