f765b08f86e5780db11e14631e47f9a67ae7f5c3
Sabin Plaiasu <sabin.pla@gmail.com>
2021-04-12 23:21:23 -0400

    restructured eclipse files

--- src/remote_procedure_events/ElevatorMotorEvent.java
+++ src/remote_procedure_events/ElevatorMotorEvent.java
@@ -10,11 +10,13 @@ public class ElevatorMotorEvent extends TimeEvent {
 
     private int destinationFloor;
     private int elevatorNumber;
+    private boolean doorError;
 
-    public ElevatorMotorEvent(Instant eventInstant, int elevatorNumber, int destinationFloor) {
+    public ElevatorMotorEvent(Instant eventInstant, int elevatorNumber, int destinationFloor, boolean doorError) {
         super(eventInstant);
         this.elevatorNumber = elevatorNumber;
         this.destinationFloor = destinationFloor;
+        this.doorError = doorError;
     }
 
     public int getArrivalFloor() {
@@ -28,4 +30,8 @@ public class ElevatorMotorEvent extends TimeEvent {
     public String toString() {
         return Date.from(getEventInstant()) + " elevator: " + elevatorNumber + " to floor: " + destinationFloor;
     }
+
+	public boolean getError() {
+		return doorError;
+	}
 }
